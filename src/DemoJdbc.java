c0b67e9ef7a537174e58173d64d86d4c75ca7b78
Aditya Mishra <mishraaditya595@gmail.com>
2025-04-26 12:30:54 +0530

    get data from the database

--- src/DemoJdbc.java
+++ src/DemoJdbc.java
@@ -19,7 +19,18 @@ public class DemoJdbc {
         String username = "postgres";
         String password = "1209";
         Connection connection = DriverManager.getConnection(url, username, password);
-
         System.out.println("====connection established====");
+
+        //create and execute statement
+        String sql = "select name from student where sid = 1";
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(sql);
+        resultSet.next();
+        String name = resultSet.getString("name");
+        System.out.println("Name: "+name);
+
+        //close connection
+        connection.close();
+        System.out.println("====connection closed====");
     }
 }
