bdd40812088ab5ee30370e78ade45a914797b12d
Aditya Mishra <mishraaditya595@gmail.com>
2025-04-26 17:07:55 +0530

    insert data using prepared statements

--- src/DemoJdbc.java
+++ src/DemoJdbc.java
@@ -33,15 +33,36 @@ public class DemoJdbc {
         displayData(statement);
         insertData(statement);
 
+
         //update data
         displayData(statement);
         updateData(statement);
+
+
+        displayData(statement);
+        //insert data using prepared statement
+        insertDataUsingPreparedStatement(connection);
 
         displayData(statement);
 
         //close connection
         connection.close();
         System.out.println("====connection closed====");
+    }
+
+    private static void insertDataUsingPreparedStatement(Connection connection) throws SQLException {
+        int sid = 5;
+        String name = "Ravi";
+        int marks = 37;
+
+        String query = "insert into student values (?,?,?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setInt(1, sid);
+        preparedStatement.setString(2, name);
+        preparedStatement.setInt(3, marks);
+        preparedStatement.execute();
+
+        System.out.println("Inserted data using prepared statements");
     }
 
     private static void displayData(Statement statement) throws SQLException {
