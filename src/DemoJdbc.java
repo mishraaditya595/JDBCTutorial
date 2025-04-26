701680692660890643ab10b18ce051dcb66d401a
Aditya Mishra <mishraaditya595@gmail.com>
2025-04-26 12:19:21 +0530

    load driver and create connection

--- src/DemoJdbc.java
+++ src/DemoJdbc.java
@@ -1,11 +1,25 @@
+//import package
+import java.sql.*;
 public class DemoJdbc {
     /*
         import package
-        load and register
+        load and register driver
         create connection
         create statement
         execute statement
         process result
         close
      */
+    public static void main(String[] args) throws Exception {
+        //load and register driver
+        Class.forName("org.postgresql.Driver");
+
+        //create connection
+        String url = "jdbc:postgresql://localhost:5432/demo";
+        String username = "postgres";
+        String password = "1209";
+        Connection connection = DriverManager.getConnection(url, username, password);
+
+        System.out.println("====connection established====");
+    }
 }
