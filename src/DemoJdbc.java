e42f738cec82c96f43e54261afc2ad76489edc02
Aditya Mishra <mishraaditya595@gmail.com>
2025-04-26 12:10:12 +0530

    Initial commit

---
+++ src/DemoJdbc.java
@@ -0,0 +1,11 @@
+public class DemoJdbc {
+    /*
+        import package
+        load and register
+        create connection
+        create statement
+        execute statement
+        process result
+        close
+     */
+}
